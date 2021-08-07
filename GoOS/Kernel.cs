a73b780e3ba78fa703f3aa3904415b6b9ce07edb
Owen Benyon <fulptdm@gmail.com>
2021-08-07 22:11:21 +0100

    55

--- GoOS/Kernel.cs
+++ GoOS/Kernel.cs
@@ -137,23 +137,7 @@ namespace GoOS
 
             if (input.ToLower() == "help")
             {
-                //
-                Console.WriteLine("");
-                Console.WriteLine("------------------------------------------");
-                Console.WriteLine("----------------GoOS Help-----------------");
-                Console.WriteLine("------------------------------------------");
-                Console.WriteLine("- here is the list of commands:          -");
-                Console.WriteLine("- Help - Shows this page                 -");
-                Console.WriteLine("- credits - shows the developers         -");
-                Console.WriteLine("- clear - clears the text                -");
-                Console.WriteLine("- sysinf - Shows system info             -");
-                Console.WriteLine("- shutdown - Shuts down GoOS             -");
-                Console.WriteLine("- ipconfig - shows your local ip         -");
-                Console.WriteLine("- gocalc - GoOS calculator   (Disabled)  -");
-                Console.WriteLine("- dir - Shows files and folders          -");
-                Console.WriteLine("- readfile - Allows you to read files    -");
-                Console.WriteLine("------------------------------------------");
-                Console.WriteLine(" ");
+                Help.help();
             }
             else if (input.ToLower() == "credits")
             {
@@ -193,9 +177,7 @@ namespace GoOS
             }
             else if (input.ToLower() == "shutdown")
             {
-                Console.WriteLine("System shutting down...");
-                Console.WriteLine("Goodbye!");
-                Cosmos.System.Power.Shutdown();
+                Shutdown.ShutdownCommand();
             }
             else if (input.ToLower() == "support")
             {
