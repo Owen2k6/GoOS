e9b0c23933ef553932bf125c0c3a47e4754e31df
Owen Benyon <fulptdm@gmail.com>
2021-09-28 18:16:28 +0100

    Update 2.0 in development

--- GoOS/Kernel.cs
+++ GoOS/Kernel.cs
@@ -44,46 +44,19 @@ namespace GoOS
         public static VGAScreen VScreen = new VGAScreen();
         private static readonly CosmosVFS cosmosVFS = new Sys.FileSystem.CosmosVFS();
         private readonly Sys.FileSystem.CosmosVFS fs = cosmosVFS;
-        Canvas canvas;
 
         protected override void BeforeRun()
         {
-            NetworkDevice nic = NetworkDevice.GetDeviceByName("eth0"); //get network device by name
-            IPConfig.Enable(nic, new Address(192, 168, 1, 69), new Address(255, 255, 255, 0), new Address(192, 168, 1, 254)); //enable IPv4 configuration
-            using (var xClient = new DHCPClient())
-            {
-                /** Send a DHCP Discover packet **/
-                //This will automatically set the IP config after DHCP response
-                xClient.SendDiscoverPacket();
-            }
-            using (var xClient = new DnsClient())
-            {
-                xClient.Connect(new Address(192, 168, 1, 254)); //DNS Server address
-
-                /** Send DNS ask for a single domain name **/
-                xClient.SendAsk("github.com");
-
-                /** Receive DNS Response **/
-                Address destination = xClient.Receive(); //can set a timeout value
-
-
-            }
-            CosmosVFS fs = new Sys.FileSystem.CosmosVFS();
-            Sys.FileSystem.VFS.VFSManager.RegisterVFS(fs);
+            String prefix = @"0:\ ";
+            var textscr = Cosmos.HAL.Global.TextScreen;
+            Cosmos.System.Global.Console = new Cosmos.System.Console(textscr);
+            Cosmos.HAL.Global.TextScreen = textscr;
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.Clear();
-            Cosmos.HAL.Global.TextScreen.SetColors(ConsoleColor.Black, ConsoleColor.White);
-            Console.WriteLine("   Goplex Studios - GoOS");
-            Console.WriteLine("   Version 1.3 Dev");
-            Console.WriteLine("   ");
-            Console.WriteLine("   Type HELP for a list of commands");
-            Console.WriteLine("   Type SUPPORT for support links...");
-            Console.WriteLine(" ");
-            var drive = new DriveInfo("0");
-            Console.WriteLine("   Volume in drive 0 is " + $"{drive.VolumeLabel}");
-            Console.WriteLine("   Drive " + @"0:\ " + "booted successfuly!");
-            Console.WriteLine("   " + $"{drive.TotalSize}" + " bytes");
-            Console.WriteLine("   " + $"{drive.AvailableFreeSpace}" + " bytes free");
-            Console.Write(@"0:\ ");
+            Console.Write(prefix);
+
+            
+            Console.WriteLine("Goplex Studios - GoOS Ver. 2.0");
         }
 
         protected override void Run()
@@ -93,36 +66,20 @@ namespace GoOS
 
 
 
-            if (input.ToLower() == "setup")
+            if (input.ToLower() == "help")
             {
-                if (!File.Exists(@"0:\setupdone"))
-                {
-                    Console.WriteLine("Welcome to GoOS!");
-                    Console.WriteLine("This is an installer to help you get the essential apps for GoOS! ");
-                    Console.WriteLine("Press Any Key to enter the setup");
-                    Console.WriteLine("Press Ctrl+C to close GoOS");
-                    Console.ReadKey();
-                    var file_stream = File.Create(@"0:\setupdone");
-                }
-                else
-                {
-                    Console.WriteLine("Looks like GoOS is already installed!");
-                }
+                Console.WriteLine("~~~GoOS Help~~~");
             }
-            else if (input.ToLower() == "dir")
+            else if (input.ToLower() == "sysinf")
             {
-                var directory_list = Sys.FileSystem.VFS.VFSManager.GetDirectoryListing("0:/");
-                foreach (var directoryEntry in directory_list)
-                {
-                    Console.WriteLine(directoryEntry.mName);
-                }
             }
             else
             {
                 Console.WriteLine("sorry, but `" + input + "` is not a command");
                 Console.WriteLine("type HELP for a list of commands");
-            }
                 Console.Write(prefix);
+            }
+                
             }
         }
     } 
