23673b2eab4371a8c4e7c5b863205661e62ad483
Owen2k6 <fulptdm@gmail.com>
2022-04-21 14:02:39 +0100

    GoOS Core UI Data import

--- GoOS/Kernel.cs
+++ GoOS/Kernel.cs
@@ -26,11 +26,70 @@ using Cosmos.Common.Extensions;
 using Cosmos.Common;
 using Cosmos.Core.Memory;
 using Cosmos.Core.IOGroup;
+using System.Drawing;
+using Cosmos.System.Graphics;
 
 namespace GoOS
 {
     public class Kernel : Sys.Kernel
     {
+
+        // GO OS UI CONFIGURATION
+        private static int screenX = 800;
+        private static int screenY = 640;
+        private static Color[] pixelBuffer = new Color[(screenX * screenY) + screenX];
+        private static Color[] pixelBufferOld = new Color[(screenX * screenY) + screenX];
+        private static Canvas canvas;
+        public static void init()
+        {
+            canvas = FullScreenCanvas.GetFullScreenCanvas();
+            canvas.Mode = new Mode(screenX, screenY, ColorDepth.ColorDepth32);
+            Cosmos.System.MouseManager.ScreenWidth = Convert.ToUInt32(screenX);
+            Cosmos.System.MouseManager.ScreenHeight = Convert.ToUInt32(screenY);
+        }
+        public static void setPixel(int x, int y, Color c)
+        {
+            if (x > screenX || y > screenY) return;
+            pixelBuffer[(x * y) + x] = c;
+        }
+        public static void drawScreen()
+        {
+            Pen pen = new Pen(Color.Orange);
+            for (int y = 0, h = screenY; y < h; y++)
+            {
+                for (int x = 0, w = screenX; x < w; x++)
+                {
+                    if (!(pixelBuffer[(y * x) + x] == pixelBufferOld[(y * y) + x]))
+                    {
+                        pen.Color = pixelBuffer[(y * screenX) + x];
+                        canvas.DrawPoint(pen, x, y);
+                    }
+                }
+            }
+            for (int i = 0, len = pixelBuffer.Length; i < len; i++)
+            {
+                pixelBuffer[i] = pixelBufferOld[i];
+            }
+        }
+        public static void clearScreen(Color c)
+        {
+            for (int i = 0, len = pixelBuffer.Length; i < len; i++)
+            {
+                pixelBuffer[i] = c;
+            }
+        }
+        public static void update()
+        {
+            clearScreen(Color.Blue);
+            setPixel(1, 1, Color.Black);
+            setPixel(1, 2, Color.Black);
+            setPixel(2, 1, Color.Black);
+            setPixel(2, 2, Color.Black);
+            drawScreen();
+            setPixel(Convert.ToInt32(Cosmos.System.MouseManager.X), Convert.ToInt32(Cosmos.System.MouseManager.Y), Color.White);
+        }
+        // END OF CONFIG
+
         private static string request = string.Empty;
         private static TcpClient tcpc = new TcpClient(80);
         private static Address dns = new Address(8, 8, 8, 8);
@@ -432,6 +491,10 @@ namespace GoOS
                 Console.ForegroundColor = ConsoleColor.Blue;
                 Console.WriteLine("TEXTADVENTURES - Text based adventure game because why not");
                 Console.ForegroundColor = ConsoleColor.Green;
+            }
+            else if (input == "ui")
+            {
+                update();
             }
 
             //Games Section
