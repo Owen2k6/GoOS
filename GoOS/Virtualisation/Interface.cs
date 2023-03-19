f6ff9a50a0041507246e06c35ec2aa2af771a7e5
Owen2k6 <fulptdm@gmail.com>
2023-03-19 12:17:16 +0000

    Interface VM

--- GoOS/Virtualisation/Interface.cs
+++ GoOS/Virtualisation/Interface.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GoOS.Virtualisation
 {
+    //This is not to be used. mainly to create a basic image for developers to clone into their own .cs file and produce a "virtual" OS
+    //You will have to modify a ton of shit from the OS in order to work with this interface.
+    //A shutdown command is highly reccomended and should issue runmode = false;
     internal class Interface
     {
+        public static bool runmode = false;
+        public static void boot(string rootpath)
+        {
+            //BeforeRun bullshit goes here
+            runmode = true;
+            run(rootpath);
+        }
+        public static void run(string rtp) {
+            
+            while (runmode)
+            {
+                //run contents in here
+            }
+        
+        }
     }
 }
