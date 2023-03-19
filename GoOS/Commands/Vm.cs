f945c69301ddad2098c678a1e6c2909c1e36f968
Owen2k6 <fulptdm@gmail.com>
2023-03-19 12:58:34 +0000

    yes i did

---
+++ GoOS/Commands/Vm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoOS.Commands
+{
+    internal class Vm
+    {
+        public static void command(string args) {
+
+        }
+    }
+}
