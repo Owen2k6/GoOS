2e54e0e677207c8fd8ca633a11e81e9b7103dc78
MrDumbrava <109512837+9xbt@users.noreply.github.com>
2023-11-06 22:11:19 +0100

    ertv ergv

--- GoOS/GUI/Apps/GoStore/MainFrame.cs
+++ GoOS/GUI/Apps/GoStore/MainFrame.cs
@@ -19,6 +19,7 @@ namespace GoOS.GUI.Apps.GoStore
         readonly string[] repos =
         {
             "apps.goos.owen2k6.com",
+            //"dev.goos.ekeleze.org",
             "repo.mobren.net"
         };
 
