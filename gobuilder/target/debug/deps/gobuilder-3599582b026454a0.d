df8e15393cc99231b4578beb72d00b033015d51b
ekeleze <78444619+ekeleze@users.noreply.github.com>
2025-08-21 17:00:18 -0500

    kernel configs and gobuilder project

---
+++ gobuilder/target/debug/deps/gobuilder-3599582b026454a0.d
@@ -0,0 +1,5 @@
+/home/ekeleze/Projects/GoOS/gobuilder/target/debug/deps/gobuilder-3599582b026454a0.d: src/main.rs
+
+/home/ekeleze/Projects/GoOS/gobuilder/target/debug/deps/libgobuilder-3599582b026454a0.rmeta: src/main.rs
+
+src/main.rs:
---
+++ gobuilder/target/debug/deps/gobuilder-e24ad5baa080ff48.d
@@ -0,0 +1,5 @@
+/home/ekeleze/Projects/GoOS/gobuilder/target/debug/deps/gobuilder-e24ad5baa080ff48.d: src/main.rs
+
+/home/ekeleze/Projects/GoOS/gobuilder/target/debug/deps/libgobuilder-e24ad5baa080ff48.rmeta: src/main.rs
+
+src/main.rs:
