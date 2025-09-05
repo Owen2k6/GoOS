420f52d09a557c12175e5e255dbdb4a3fca5e3ea
xrc2 <109512837+9xbt@users.noreply.github.com>
2025-09-05 19:23:23 +0200

    Taskman: Fix color scheme

--- GoOS/GUI/Apps/TaskManager.cs
+++ GoOS/GUI/Apps/TaskManager.cs
@@ -20,21 +20,17 @@ public class TaskManager : Window
         Unkillable = true;
         SetDock(WindowDock.Auto);
 
-        EndButton = new Button(this, Convert.ToUInt16(Contents.Width - 90), Convert.ToUInt16(Contents.Height - 30),
-            80, 20, " End task ") { Clicked = EndButton_Click };
-        AboutButton =
-            new Button(this, Convert.ToUInt16(Contents.Width - 124), Convert.ToUInt16(Contents.Height - 30), 24, 20,
-                "?") { Clicked = ShowAboutDialog };
-        Windows = new List(this, 10, 10, Convert.ToUInt16(Contents.Width - 20),
-            Convert.ToUInt16(Contents.Height - 60), "Processes", Array.Empty<string>());
+        EndButton = new Button(this, Convert.ToUInt16(Contents.Width - 90), Convert.ToUInt16(Contents.Height - 30), 80, 20, " End task ") { Clicked = EndButton_Click };
+        AboutButton = new Button(this, Convert.ToUInt16(Contents.Width - 124), Convert.ToUInt16(Contents.Height - 30), 24, 20, "?") { Clicked = () => { ShowAboutDialog("1.0"); } };
+        Windows = new List(this, 10, 10, Convert.ToUInt16(Contents.Width - 20), Convert.ToUInt16(Contents.Height - 60), "Processes", Array.Empty<string>());
 
         WindowManager.TaskmanHook = Update;
 
         // Render the buttons.
-        Contents.Clear(Color.LightGray);
+        Contents.Clear(Color.White);
         RenderSystemStyleBorder();
         Contents.DrawFilledRectangle(2, Convert.ToUInt16(Contents.Height - 40),
-            Convert.ToUInt16(Contents.Width - 4), 38, 0, Color.DeepGray);
+            Convert.ToUInt16(Contents.Width - 4), 38, 0, new Color(0xFFCCCCCC));
         AboutButton.Render();
         EndButton.Render();
     }
--- GoOS/GUI/Controls/List.cs
+++ GoOS/GUI/Controls/List.cs
@@ -1,89 +1,86 @@
 ﻿using System;
 using Cosmos.System;
-using Gold.Graphics;
 using GoOS.GUI.Models;
+using Gold.Graphics;
 
-namespace GoOS.GUI;
-
-public class List : Control
+namespace GoOS.GUI
 {
-    public Color BackgroundColor = new(143, 143, 143);
-
-    public Color ForegroundColor = Color.White;
-    public string[] Items;
-
-    public int Selected;
-    public string Title;
-
-    public List(Window parent, ushort x, ushort y, ushort width, ushort height, string title, string[] items)
-        : base(parent, x, y, width, height)
+    public class List : Control
     {
-        Items = items;
-        Title = title;
-        Selected = 0;
-    }
+        public string[] Items;
+        public string Title;
 
-    public override void Render()
-    {
-        if (Selected < 0)
-            Selected = 0;
-        else if (Selected > Items.Length) Selected = Items.Length - 1;
+        public int Selected;
 
-        Contents.Clear(BackgroundColor);
+        public Color ForegroundColor = Color.Black;
+        public Color BackgroundColor = Color.White;
 
-        // Background.
-        Contents.Clear(Color.LightGray);
-
-        // Dark shadow.
-        Contents.DrawLine(0, 0, Contents.Width - 1, 0, Color.Black);
-        Contents.DrawLine(0, 0, 0, Contents.Height - 1, Color.Black);
-
-        // Highlight.
-        Contents.DrawLine(1, Contents.Height - 2, Contents.Width - 2, Contents.Height - 2,
-            new Color(130, 130, 130));
-        Contents.DrawLine(Contents.Width - 2, 1, Contents.Width - 2, Contents.Height - 1, new Color(130, 130, 130));
-
-        // Light highlight.
-        Contents.DrawLine(0, Contents.Height - 1, Contents.Width, Contents.Height - 1, Color.White);
-        Contents.DrawLine(Contents.Width - 1, 0, Contents.Width - 1, Contents.Height - 1, Color.White);
-
-        //So much for saying your code is "dry". this is from Button.cs - Owen2k6 :^(
-        //That was atmo's :skull: nmf
-
-        if (Title != "")
+        public List(Window parent, ushort x, ushort y, ushort width, ushort height, string title, string[] items)
+            : base(parent, x, y, width, height)
         {
-            // Title
-            Contents.DrawLine(0, 19, Contents.Width - 2, 19, new Color(216, 216, 216));
-            /*Contents.DrawLine(0, 20, Contents.Width - 2, 19, new Color(216, 216, 216));
-            Contents.DrawLine(0, 21, Contents.Width - 2, 19, new Color(216, 216, 216)); // fuck you owen*/
-            Contents.DrawString(2, 2, Title, Resources.Font_1x, ForegroundColor);
+            Items = items;
+            Title = title;
+            Selected = 0;
         }
 
-        // Items
-        for (var i = 0; i < Items.Length; i++)
-            if (i == Selected)
+        public override void Render()
+        {
+            if (Selected < 0)
             {
-                Contents.DrawFilledRectangle(1, 20 + i * 20, Convert.ToUInt16(Contents.Width - 3), 18, 0,
-                    new Color(130, 130, 130));
-                //TODO: XRC you fucking dumbass label areas where you force color changes.
-                //TODO: For example, you could just simply put //Creates a WHITE rectangle.
-                //TODO: This fucked up my theme changing work just cus you made it unnoticeable.
-                //-Owen2k6 >:^(
-                Contents.DrawString(2, 21 + i * 20, Items[i], Resources.Font_1x, Color.Black);
+                Selected = 0;
             }
-            else
+            else if (Selected > Items.Length)
             {
-                Contents.DrawString(2, 21 + i * 20, Items[i], Resources.Font_1x, ForegroundColor);
+                Selected = Items.Length - 1;
             }
 
-        Parent.RenderControls();
-    }
+            Contents.Clear(BackgroundColor);
 
-    internal override void HandleDown(MouseEventArgs args)
-    {
-        if (IsMouseOver) Selected = ((int)MouseManager.Y - Parent.Y - Window.TITLE_BAR_HEIGHT - Y) / 20 - 1;
+            // Background.
+            Contents.Clear(Color.White);
 
-        Render();
-        Parent.RenderControls();
+            // Dark shadow.
+            Contents.DrawLine(0, 0, Contents.Width - 1, 0, Color.Black);
+            Contents.DrawLine(0, 0, 0, Contents.Height - 1, Color.Black);
+
+            // Highlight.
+            Contents.DrawLine(1, Contents.Height - 2, Contents.Width - 2, Contents.Height - 2, new Color(216, 216, 216));
+            Contents.DrawLine(Contents.Width - 2, 1, Contents.Width - 2, Contents.Height - 1, new Color(216, 216, 216));
+
+            // Light highlight.
+            Contents.DrawLine(0, Contents.Height - 1, Contents.Width, Contents.Height - 1, Color.White);
+            Contents.DrawLine(Contents.Width - 1, 0, Contents.Width - 1, Contents.Height - 1, Color.White);
+
+            if (Title != "")
+            {
+                // Title
+                Contents.DrawLine(1, 19, Contents.Width - 2, 19, new Color(216, 216, 216));
+                Contents.DrawString(2, 2, Title, Resources.Font_1x, ForegroundColor);
+            }
+
+            // Items
+            for (int i = 0; i < Items.Length; i++)
+            {
+                if (i == Selected)
+                {
+                    Contents.DrawFilledRectangle(1, 20 + (i * 20), Convert.ToUInt16(Contents.Width - 3), 18, 0, new Color(234, 234, 234));
+                }
+
+                Contents.DrawString(2, 21 + (i * 20), Items[i], Resources.Font_1x, ForegroundColor);
+            }
+
+            Parent.RenderControls();
+        }
+
+        internal override void HandleDown(MouseEventArgs args)
+        {
+            if (IsMouseOver)
+            {
+                Selected = ((int)MouseManager.Y - Parent.Y + Window.TITLE_BAR_HEIGHT + Y) / 20 - 4;
+            }
+
+            Render();
+            Parent.RenderControls();
+        }
     }
 }
