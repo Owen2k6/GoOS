dc76f07751aefe46dfbaa3c5905aeeaf5d69a59f
Owen2k6 <owen@owen2k6.com>
2025-09-05 20:03:37 +0100

    Handicap the betterconsole just a bit more

--- GoOS/BetterConsole/BetterConsole.cs
+++ GoOS/BetterConsole/BetterConsole.cs
@@ -5,10 +5,8 @@ using Cosmos.Core.Memory;
 using Cosmos.System;
 using Gold.Graphics;
 using GoOS.GUI;
-using GoOS.GUI.Apps.Settings;
-using GoOS.Themes;
 using static GoOS.Resources;
-using Kernel = GoOS.Kernel;
+
 public static class BetterConsole
 {
     public const ushort CharWidth = 8;
@@ -27,21 +25,25 @@ public static class BetterConsole
     public static bool DoubleBufferedMode;
     public static Queue<KeyEvent> KeyBuffer = new();
     public static string Title = "BetterConsole";
-    private static string lastInput = string.Empty;
     private static byte scrollCounter;
+
     private struct Cell
     {
         public char Ch;
         public Color Fg;
         public Color Bg;
     }
+
     private static Cell[] _cells = Array.Empty<Cell>();
+
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     private static int Index(int x, int y) => y * WindowWidth + x;
+
     private static readonly string[] _charLut = new string[256];
     private static bool _cursorDrawn;
     private static int _cursorLeftDrawn = -1;
     private static int _cursorTopDrawn = -1;
+
     public static void Init(ushort width, ushort height)
     {
         Canvas = new Canvas(width, height);
@@ -63,6 +65,7 @@ public static class BetterConsole
             _cells[i].Fg = fg;
         }
     }
+
     public static void Clear(bool render = true)
     {
         Canvas.Clear(BackgroundColor);
@@ -77,13 +80,16 @@ public static class BetterConsole
             _cells[i].Bg = bg;
             _cells[i].Fg = fg;
         }
+
         if (render || !DoubleBufferedMode)
             Render();
     }
+
     public static void Render()
     {
         WindowManager.Update();
     }
+
     public static void Write(object text, bool quick = false)
     {
         if (text == null) return;
@@ -100,11 +106,13 @@ public static class BetterConsole
                 CheckNewline();
                 continue;
             }
+
             if (c == '\r')
             {
                 CursorLeft = 0;
                 continue;
             }
+
             if (c == '\t')
             {
                 int spaces = TabWidth - (CursorLeft % TabWidth);
@@ -113,15 +121,19 @@ public static class BetterConsole
                     PutChar(' ', CursorLeft, CursorTop, quick);
                     AdvanceCursor();
                 }
+
                 continue;
             }
+
             PutChar(c, CursorLeft, CursorTop, quick);
             AdvanceCursor();
         }
+
         if (!DoubleBufferedMode)
             Render();
         SmartCursor(true);
     }
+
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     private static void AdvanceCursor()
     {
@@ -136,12 +148,14 @@ public static class BetterConsole
             CursorLeft++;
         }
     }
+
     public static void WriteLine(object text = null, bool quick = false)
     {
         if (text != null)
             Write(text, quick);
         Write("\n", quick);
     }
+
     public static ConsoleKeyInfo ReadKey(bool intercept = true)
     {
         while (true)
@@ -151,6 +165,7 @@ public static class BetterConsole
                 SmartCursor(true);
                 if (!DoubleBufferedMode) Render();
             }
+
             if (KeyBuffer.Count > 0)
             {
                 var key = KeyBuffer.Dequeue();
@@ -160,7 +175,9 @@ public static class BetterConsole
                     char ch = key.KeyChar;
                     if (ch == '\n')
                     {
-                        CursorLeft = 0; CursorTop++; CheckNewline();
+                        CursorLeft = 0;
+                        CursorTop++;
+                        CheckNewline();
                     }
                     else if (ch == '\r')
                     {
@@ -169,7 +186,11 @@ public static class BetterConsole
                     else if (ch == '\t')
                     {
                         int spaces = TabWidth - (CursorLeft % TabWidth);
-                        for (int t = 0; t < spaces; t++) { PutChar(' ', CursorLeft, CursorTop, true); AdvanceCursor(); }
+                        for (int t = 0; t < spaces; t++)
+                        {
+                            PutChar(' ', CursorLeft, CursorTop, true);
+                            AdvanceCursor();
+                        }
                     }
                     else if (ch >= ' ')
                     {
@@ -177,6 +198,7 @@ public static class BetterConsole
                         AdvanceCursor();
                     }
                 }
+
                 var xShift = (key.Modifiers & ConsoleModifiers.Shift) == ConsoleModifiers.Shift;
                 var xAlt = (key.Modifiers & ConsoleModifiers.Alt) == ConsoleModifiers.Alt;
                 var xControl = (key.Modifiers & ConsoleModifiers.Control) == ConsoleModifiers.Control;
@@ -187,6 +209,7 @@ public static class BetterConsole
             TrimKeyBuffer();
         }
     }
+
     public static string ReadLine()
     {
         int startX = CursorLeft, startY = CursorTop;
@@ -199,6 +222,7 @@ public static class BetterConsole
                 SmartCursor(true);
                 if (!DoubleBufferedMode) Render();
             }
+
             if (KeyBuffer.Count > 0)
             {
                 var key = KeyBuffer.Dequeue();
@@ -209,7 +233,6 @@ public static class BetterConsole
                         CursorLeft = 0;
                         CursorTop++;
                         CheckNewline();
-                        lastInput = line;
                         reading = false;
                         break;
                     case ConsoleKeyEx.Backspace:
@@ -226,86 +249,24 @@ public static class BetterConsole
                                 CursorLeft--;
                                 PutChar(' ', CursorLeft, CursorTop, true);
                             }
+
                             if (line.Length > 0)
                                 line = line.Remove(line.Length - 1);
                         }
-                        break;
-                    case ConsoleKeyEx.Tab:
-                        {
-                            int spaces = TabWidth - (CursorLeft % TabWidth);
-                            for (int t = 0; t < spaces; t++)
-                            {
-                                PutChar(' ', CursorLeft, CursorTop, true);
-                                AdvanceCursor();
-                            }
-                            line += new string(' ', spaces);
-                        }
-                        break;
-                    case ConsoleKeyEx.UpArrow:
-                        if (!string.IsNullOrEmpty(lastInput))
-                        {
-                            SetCursorPosition(startX, startY);
-                            int toClear = line.Length;
-                            for (int i = 0; i < toClear; i++)
-                            {
-                                PutChar(' ', CursorLeft, CursorTop, true);
-                                AdvanceCursor();
-                            }
-                            SetCursorPosition(startX, startY);
-                            Write(lastInput, true);
-                            line = lastInput;
-                        }
+
                         break;
                     default:
-                        if (KeyboardManager.ControlPressed)
+                        char ch = key.KeyChar;
+                        if (ch >= ' ')
                         {
-                            if (key.Key == ConsoleKeyEx.G)
-                            {
-                                var collected = Heap.Collect() + " items collected";
-                                Canvas.DrawString(Canvas.Width - collected.Length * 8 - 8, Canvas.Height - 32,
-                                    collected, Font_1x, ThemeManager.WindowText);
-                                SetCursorPosition(startX, startY);
-                                Write(line, true);
-                            }
-                            else if (key.Key == ConsoleKeyEx.L)
-                            {
-                                Clear();
-                                line = string.Empty;
-                                reading = false;
-                            }
-                            else if (KeyboardManager.ShiftPressed && key.Key == ConsoleKeyEx.E)
-                            {
-                                Write("> ", true);
-                                var input = ReadLine();
-                                if (input == "e015")
-                                {
-                                    Clear();
-                                    WriteLine("Thank you for using GoOS - Owen2k6");
-                                    WriteLine("There used to be an image here however");
-                                    WriteLine("due to Console resizing it had to be");
-                                    WriteLine("removed.");
-                                    WriteLine("Check the source code on github to see it!");
-                                    WriteLine("Big thanks to all who helped develop GoOS");
-                                    WriteLine("Over the years. Great people!");
-                                    WriteLine("- Owen2k6");
-                                    WriteLine("Press enter twice to escape!");
-                                    ReadKey();
-                                    Clear();
-                                }
-                            }
+                            PutChar(ch, CursorLeft, CursorTop, true);
+                            AdvanceCursor();
+                            line += ch;
                         }
-                        else
-                        {
-                            char ch = key.KeyChar;
-                            if (ch >= ' ')
-                            {
-                                PutChar(ch, CursorLeft, CursorTop, true);
-                                AdvanceCursor();
-                                line += ch;
-                            }
-                        }
+
                         break;
                 }
+
                 if (!DoubleBufferedMode) Render();
             }
             else
@@ -314,23 +275,29 @@ public static class BetterConsole
                 TrimKeyBuffer();
             }
         }
+
         return line;
     }
+
     public static void SetCursorPosition(int x, int y)
     {
         CursorLeft = Math.Clamp(x, 0, WindowWidth - 1);
         CursorTop = Math.Clamp(y, 0, WindowHeight - 1);
-        _cursorDrawn = false; // force re-draw at new location when requested
+        _cursorDrawn = false;
     }
+
     public static (int Left, int Top) GetCursorPosition() => (CursorLeft, CursorTop);
     public static void Beep(uint freq = 800, uint duration = 125) => PCSpeaker.Beep(freq, duration);
+
     #region Private
+
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     internal static void TrimKeyBuffer()
     {
         while (KeyBuffer.Count > MaxKeyBufferSize)
             KeyBuffer.Dequeue();
     }
+
     internal static void SmartCursor(bool on)
     {
         if (!CursorVisible) return;
@@ -354,6 +321,7 @@ public static class BetterConsole
             _cursorDrawn = false;
         }
     }
+
     private static void CheckNewline()
     {
         if (CursorTop >= WindowHeight)
@@ -372,6 +340,7 @@ public static class BetterConsole
                 _cells[start + i].Bg = bg;
                 _cells[start + i].Fg = fg;
             }
+
             CursorTop = WindowHeight - 1;
             if (!DoubleBufferedMode)
                 Render();
@@ -381,6 +350,7 @@ public static class BetterConsole
                 Heap.Collect();
                 scrollCounter = 0;
             }
+
             _cursorDrawn = false;
         }
     }
@@ -396,6 +366,7 @@ public static class BetterConsole
             _cursorDrawn = false;
             return;
         }
+
         int px = x * CharWidth;
         int py = y * CharHeight;
         if (!cell.Bg.Equals(BackgroundColor) || cell.Ch == ' ' || c == ' ')
@@ -405,6 +376,7 @@ public static class BetterConsole
             string glyph = (c <= 255) ? _charLut[c] : new string(c, 1);
             Canvas.DrawString(px, py, glyph, Font_1x, ForegroundColor);
         }
+
         cell.Ch = c;
         cell.Fg = ForegroundColor;
         cell.Bg = BackgroundColor;
@@ -412,6 +384,7 @@ public static class BetterConsole
         if (!DoubleBufferedMode && quick == false)
             Render();
     }
+
     private static void RedrawCell(int x, int y)
     {
         if ((uint)x >= WindowWidth || (uint)y >= WindowHeight) return;
@@ -426,24 +399,26 @@ public static class BetterConsole
             Canvas.DrawString(px, py, glyph, Font_1x, cell.Fg);
         }
     }
+
     #endregion
 }
+
 public static class ConsoleColorEx
 {
-    public static readonly Color Black       = new(0,   0,   0);
-    public static readonly Color DarkBlue    = new(0,   0,   170);
-    public static readonly Color DarkGreen   = new(0,   170,  0);
-    public static readonly Color DarkCyan    = new(0,   170,  170);
-    public static readonly Color DarkRed     = new(170, 0,    0);
-    public static readonly Color DarkMagenta = new(170, 0,    170);
-    public static readonly Color DarkYellow  = new(170, 85,   0);
-    public static readonly Color Gray        = new(170, 170,  170);
-    public static readonly Color DarkGray    = new(85,  85,   85);
-    public static readonly Color Blue        = new(85,  85,   255);
-    public static readonly Color Green       = new(85,  255,  85);
-    public static readonly Color Cyan        = new(85,  255,  255);
-    public static readonly Color Red         = new(255, 85,   85);
-    public static readonly Color Magenta     = new(255, 85,   255);
-    public static readonly Color Yellow      = new(255, 255,  85);
-    public static readonly Color White       = new(255, 255,  255);
+    public static readonly Color Black = new(0, 0, 0);
+    public static readonly Color DarkBlue = new(0, 0, 170);
+    public static readonly Color DarkGreen = new(0, 170, 0);
+    public static readonly Color DarkCyan = new(0, 170, 170);
+    public static readonly Color DarkRed = new(170, 0, 0);
+    public static readonly Color DarkMagenta = new(170, 0, 170);
+    public static readonly Color DarkYellow = new(170, 85, 0);
+    public static readonly Color Gray = new(170, 170, 170);
+    public static readonly Color DarkGray = new(85, 85, 85);
+    public static readonly Color Blue = new(85, 85, 255);
+    public static readonly Color Green = new(85, 255, 85);
+    public static readonly Color Cyan = new(85, 255, 255);
+    public static readonly Color Red = new(255, 85, 85);
+    public static readonly Color Magenta = new(255, 85, 255);
+    public static readonly Color Yellow = new(255, 255, 85);
+    public static readonly Color White = new(255, 255, 255);
 }
